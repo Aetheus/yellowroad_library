07066f865f68425d170d63135d094cc8e83c8281
Adrian <adrianhyk@gmail.com>
2017-09-15 00:31:32 +0800

    add handler for user sign ups

--- http/routes/userRoute/handlers.go
+++ http/routes/userRoute/handlers.go
@@ -8,16 +8,51 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type signUpForm struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email string `json:"email"`
+}
+func SignUp(authService authService.AuthService) gin.HandlerFunc {
+	return func (c *gin.Context) {
+		form := signUpForm{}
+
+		if err := c.BindJSON(&form) ; err != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H { "error" : err.Error() })
+			return
+		}
+
+		user, err := authService.RegisterUser(form.Username,form.Password,form.Email)
+
+		if (err != nil) {
+			c.JSON(http.StatusUnprocessableEntity, gin.H { "error" : err.Error() })
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H {
+			"user" : user,
+		})
+		return
+	}
+}
+
+type loginForm struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 func Login(authService authService.AuthService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var username = c.GetString("username")
-		var password = c.GetString("password")
+		form := loginForm{}
+		if err := c.BindJSON(&form); err != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H { "error" : err.Error() })
+			return
+		}
 
-		user, loginToken, err := authService.LoginUser(username, password)
+		user, loginToken, err := authService.LoginUser(form.Username, form.Password)
 
 		if (err != nil){
 			//TODO : return an appropriate error code / standardize the API return format and errors with custom structs
-			c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
 			return
 		}
 
