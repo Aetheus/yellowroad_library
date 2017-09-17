69cddf9210f472ac545067f3a69030a051aea4fe
Adrian <adrianhyk@gmail.com>
2017-09-17 22:19:05 +0800

    enable db logging by default for now

--- database/Connection.go
+++ database/Connection.go
@@ -7,6 +7,7 @@ func Conn(dbType string, connectionString string) *gorm.DB {
 	var dbConn *gorm.DB
 
 	db, err := gorm.Open(dbType, connectionString)
+	db.LogMode(true)
 	if err != nil {
 		panic("Failed to connect to the database!")
 	}
