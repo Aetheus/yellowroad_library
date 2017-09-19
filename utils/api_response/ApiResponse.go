022d95c7d2f081c8cef2b520c34be2c3ed3a96a0
Adrian <adrianhyk@gmail.com>
2017-09-20 00:43:11 +0800

    add some convenience functions for Gin in the api response package

--- utils/api_response/ApiResponse.go
+++ utils/api_response/ApiResponse.go
@@ -1,6 +1,9 @@
 package api_response
 
-import "yellowroad_library/utils/app_error"
+import (
+	"yellowroad_library/utils/app_error"
+	"net/http"
+)
 
 var SUCCESS = "success"
 var FAIL = "fail"
@@ -21,6 +24,10 @@ func Success(data interface{}) ApiResponse{
 		Data : data,
 	}
 }
+//a convenience for Gin's context.JSON() func, which takes two parameters
+func SuccessWithCode(data interface{}) (int, ApiResponse) {
+	return http.StatusOK, Success(data)
+}
 
 //converts an AppError into either a Fail or Error response, depending on its HTTP code
 func ConvertErr(err app_error.AppError) ApiResponse{
@@ -32,6 +39,10 @@ func ConvertErr(err app_error.AppError) ApiResponse{
 		var dummyData struct{} //TODO add "data" field to AppError and use it here instead
 		return Fail(err.EndpointMessage(), dummyData)
 	}
+}
+//a convenience for Gin's context.JSON() func, which takes two parameters
+func ConvertErrWithCode(err app_error.AppError) (int, ApiResponse) {
+	return err.HttpCode(),ConvertErr(err)
 }
 
 func Fail(message string, data interface{}) ApiResponse{
