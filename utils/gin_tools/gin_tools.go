1778437ce3f757ce8e5178a9e58a84bc50365c48
Adrian <adrianhyk@gmail.com>
2017-09-25 00:09:19 +0800

    add gin_tools package that will include useful utility functions to make working with the Gin web framework less clunky

---
+++ utils/gin_tools/gin_tools.go
@@ -0,0 +1,18 @@
+package gin_tools
+
+import (
+	"github.com/gin-gonic/gin"
+	"yellowroad_library/utils/app_error"
+	"net/http"
+	"strconv"
+)
+
+func GetIntParam(key string,c *gin.Context) (int, app_error.AppError){
+	if value, convErr := strconv.Atoi(c.Param(key)); convErr != nil {
+		appErr := app_error.New(http.StatusUnprocessableEntity,
+			"", key + " must be a valid integer value!")
+		return 0, appErr
+	} else {
+		return value, nil
+	}
+}
