719eb713bbb262fcd58ed1285e597bad2cdc3696
Adrian <adrianhyk@gmail.com>
2017-09-25 22:21:01 +0800

    add a basic unit test for token service

---
+++ tests/services/token_serv/AppTokenService_test.go
@@ -0,0 +1,45 @@
+package token_serv
+
+import (
+	"testing"
+
+	"yellowroad_library/services/token_serv/app_token_serv"
+	"yellowroad_library/database/entities"
+)
+
+var user = 	entities.User{
+				ID : 95,
+				Username : "bob",
+				Password : "az89712,lazlkjap109asnklawjlj34",
+				Email    : "bob@mail.com",
+			}
+
+
+func TestCreateTokenString(t *testing.T) {
+	appTokenServ := app_token_serv.New()
+	tokenString, err := appTokenServ.CreateTokenString(user)
+	if err != nil {
+		t.Errorf("Error occured:\n -Context Message: %s\n - EndpointMessage: %s", err.Error(), err.EndpointMessage())
+	}
+
+	if len(tokenString) == 0 {
+		t.Errorf("Token string should not have a lenght of zero!")
+	}
+
+}
+
+func TestValidateTokenString(t *testing.T) {
+	appTokenServ := app_token_serv.New()
+	tokenString, _ := appTokenServ.CreateTokenString(user)
+
+	claim, err := appTokenServ.ValidateTokenString(tokenString)
+	if err != nil {
+		t.Errorf("Error occured:\n -Context Message: %s\n - EndpointMessage: %s",
+			err.Error(), err.EndpointMessage())
+	}
+
+	if claim.UserID != user.ID {
+		t.Errorf("The claim's user id of [%d] does not match the expected user ID of [%d]!",
+			claim.UserID, user.ID)
+	}
+}
