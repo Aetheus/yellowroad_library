508515e04cda95c2d242a61e3c079096022be083
Adrian <adrianhyk@gmail.com>
2017-10-01 02:07:18 +0800

    add a jsonb struct for use with gorm for now

---
+++ utils/Jsonb.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"encoding/json"
+	"errors"
+)
+
+type Jsonb struct {
+	json.RawMessage
+}
+
+func (j Jsonb) Value() (driver.Value, error) {
+	return j.MarshalJSON()
+}
+
+func (j *Jsonb) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	return json.Unmarshal(bytes, j)
+}
