096d3bd952bb39b22958bb29c458f2386c977655
Adrian <adrianhyk@gmail.com>
2017-11-17 02:38:52 +0800

    navigatetochapter will now apply the chapter path's effects to the current save and return a new save!

--- services/story_serv/DefaultStoryService.go
+++ services/story_serv/DefaultStoryService.go
@@ -37,12 +37,6 @@ func (this DefaultStoryService) NavigateToChapter(request PathRequest, encodedSa
 
 	destinationChapterIsFirstChapter := (destinationChapter.ID == destinationChapter.Book.FirstChapterId.Int)
 
-	//if the save is empty and we're not on the first chapter, then consider ourselves to be in freemode
-	if (request.IsFreeMode || saveIsEmpty && !destinationChapterIsFirstChapter) {
-		response := NewPathResponse(destinationChapter,story_save.Save{})
-		return response, nil
-	}
-
 	var currentSave story_save.Save
 	if (saveIsEmpty) {
 		currentSave = story_save.New()
@@ -53,7 +47,27 @@ func (this DefaultStoryService) NavigateToChapter(request PathRequest, encodedSa
 		}
 	}
 
-	//TODO: actually do something here - get the chapter path, use it to validate/update the story save, etc
+	//if the save is empty and we're not on the first chapter, then consider ourselves to be in freemode
+	if (request.IsFreeMode || saveIsEmpty && !destinationChapterIsFirstChapter) {
+		response := NewPathResponse(destinationChapter,currentSave)
+		return response, nil
+	}
+
+	//TODO actually check if the save matches requirements first before applying the effects
+	if request.ChapterPathId != 0 {
+		chapterPath, err :=  this.work.ChapterPathRepo().FindById(request.ChapterPathId)
+		if (err != nil){
+			return PathResponse{}, err
+		}
+
+		effect := chapterPath.Effects.ToString()
+		err = currentSave.ApplyEffect(effect)
+		if (err != nil) {
+			return PathResponse{}, err
+		}
+	}
+
+
 	response := NewPathResponse(destinationChapter,currentSave)
 	return response, nil
 }
