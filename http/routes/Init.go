626c4e3c1f7a8ad2f9c0f42cd7da3606cc786c73
Adrian <adrianhyk@gmail.com>
2017-12-02 00:50:48 +0800

    allow CORS

--- http/routes/Init.go
+++ http/routes/Init.go
@@ -2,15 +2,29 @@ package routes
 
 import (
 	"fmt"
-	"yellowroad_library/containers"
-	"yellowroad_library/http/routes/story_route"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+
+	"yellowroad_library/containers"
+	"yellowroad_library/http/routes/story_route"
 	"yellowroad_library/http/routes/user_route"
 )
 
 func Init(container containers.Container) AppRouter {
 	var ginEngine = gin.Default()
+	ginEngine.Use(cors.New(cors.Config{
+		//TODO : origins should be from a config!!
+		AllowOrigins:     []string{"http://domainname.com", "http://localhost:3000"},
+
+		AllowMethods:     []string{"PUT","PATCH","GET","POST", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
+
 	var r = newAppRouter(ginEngine, container)
 	var portString = fmt.Sprintf(":%d", container.GetConfiguration().Web.Port)
 
