e1b5bacb607aca42871ac40c150df19833b0e6cf
Adrian <adrianhyk@gmail.com>
2018-01-17 02:54:39 +0800

    reorder attributes in migration; outcome of migration itself is unchanged, this is purely cosmetic

--- database/migrations/201801160117_add_save_table.up.sql
+++ database/migrations/201801160117_add_save_table.up.sql
@@ -2,9 +2,9 @@ CREATE TABLE story_saves (
   token TEXT PRIMARY KEY,
   save  JSON,
 
-  created_at timestamp without time zone,
   created_by INT REFERENCES users(id) NULL, /* nullable since guests can also create story saves */
-
   book_id INT REFERENCES books(id),
-  chapter_id INT REFERENCES chapters(id)
+  chapter_id INT REFERENCES chapters(id),
+
+  created_at timestamp without time zone
 )
