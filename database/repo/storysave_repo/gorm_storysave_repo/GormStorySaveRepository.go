c44d40f329d6aebf4448808d3b883962f285c34d
Adrian <adrianhyk@gmail.com>
2018-01-25 00:31:14 +0800

    add a repository for the story save and a test for it as well

---
+++ database/repo/storysave_repo/gorm_storysave_repo/GormStorySaveRepository.go
@@ -0,0 +1,79 @@
+package gorm_storysave_repo
+
+import (
+	"github.com/jinzhu/gorm"
+	"yellowroad_library/database/repo/storysave_repo"
+	"yellowroad_library/database/entities"
+	"yellowroad_library/utils/app_error"
+	"net/http"
+)
+
+type GormStorySaveRepository struct {
+	db *gorm.DB
+}
+var _ storysave_repo.StorySaveRepository = GormStorySaveRepository{}
+
+func New(db *gorm.DB) GormStorySaveRepository{
+	return GormStorySaveRepository{db}
+}
+
+func (this GormStorySaveRepository) FindByToken(token string) (entities.StorySave, app_error.AppError) {
+	var save entities.StorySave
+
+	queryResult := this.db.
+						Where("token = ?", token).
+						First(&save)
+
+	if(queryResult.Error != nil) {
+		var returnedErr app_error.AppError
+		if queryResult.RecordNotFound() {
+			returnedErr = app_error.Wrap(queryResult.Error).
+				SetHttpCode(http.StatusNotFound).
+				SetEndpointMessage("No save found")
+		} else {
+			returnedErr = app_error.Wrap(queryResult.Error)
+		}
+		return save, returnedErr
+	}
+
+	return save, nil
+}
+
+func (this GormStorySaveRepository) Insert(save *entities.StorySave) (app_error.AppError) {
+	queryResult := this.db.
+						Set("gorm:save_associations", false).
+						Create(save)
+
+	if queryResult.Error != nil {
+		return app_error.Wrap(queryResult.Error)
+	}
+
+	return nil
+}
+
+func (this GormStorySaveRepository) Update(save *entities.StorySave) (app_error.AppError) {
+	queryResult := this.db.
+						Set("gorm:save_associations", false).
+						Save(save)
+
+	if queryResult.Error != nil {
+		return app_error.Wrap(queryResult.Error)
+	}
+
+	return nil
+}
+
+func (this GormStorySaveRepository) Delete(save *entities.StorySave) (app_error.AppError) {
+	queryResult := this.db.
+						Set("gorm:save_associations", false).
+						Delete(save)
+
+	if queryResult.Error != nil {
+		return app_error.Wrap(queryResult.Error)
+	}
+
+	return nil
+}
+
+
+
---
+++ database/repo/storysave_repo/gorm_storysave_repo/GormStorySaveRepository_test.go
@@ -0,0 +1,133 @@
+package gorm_storysave_repo
+
+import "testing"
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"yellowroad_library/test_utils"
+	"github.com/jinzhu/gorm"
+	"yellowroad_library/database/repo/user_repo/gorm_user_repo"
+	"yellowroad_library/database/repo/book_repo/gorm_book_repo"
+	"yellowroad_library/database/repo/chapter_repo/gorm_chapter_repo"
+	"yellowroad_library/database/entities"
+	"yellowroad_library/database"
+	"encoding/json"
+)
+
+func TestGormStorySaveRepository(t *testing.T){
+	// Only pass t into top-level Convey calls
+	Convey("Given a GormBookRepository, UserRepository and GormStorySaveRepository", t, test_utils.WithRealGormDBConnection(func(gormDB *gorm.DB){
+		transaction := gormDB.Begin()
+
+		storySaveRepo := New(transaction)
+		userRepo := gorm_user_repo.New(transaction)
+		bookRepo := gorm_book_repo.New(transaction)
+		chapterRepo := gorm_chapter_repo.New(transaction)
+
+		Convey("Creating test user, book and chapter", func (){
+			user := entities.User{
+				Username:  "Jill Bates",
+				Password:  "pass123132",
+				Email:     "jill@bates.com",
+			}
+			err := userRepo.Insert(&user)
+			So(err, ShouldBeNil)
+
+
+			book  := entities.Book{
+				Title:          "Silly Corn Valley",
+				Description:    "The place where people write Corn for Operating Cereals",
+				Permissions:    "",
+				CreatorId:      user.ID,
+			}
+			err = bookRepo.Insert(&book)
+			So(err, ShouldBeNil)
+
+
+			chapter := entities.Chapter{
+				Title:     "Have you ...",
+				Body:      "EVERRR THE HEARD THE WOLFF CRYYYY",
+				BookId:    book.ID,
+				CreatorId: user.ID,
+			}
+			err = chapterRepo.Insert(&chapter)
+			So(err, ShouldBeNil)
+
+
+			Convey("Inserting some Story Saves should work", func (){
+				rawBytes, err := json.Marshal(map[string]interface{} {
+					"have_heard_the_wolf_cry_to_the_blue_corn_moon" : true,
+				})
+				So(err, ShouldBeNil)
+
+				storySave := entities.StorySave{
+					Token:     "HaveYouEverHeardTheWolfCryToTheBlueCornMoon",
+					Save:      database.Jsonb{json.RawMessage(rawBytes)},
+					CreatedBy: user.ID,
+					BookId:    book.ID,
+					ChapterId: chapter.ID,
+				}
+				err = storySaveRepo.Insert(&storySave)
+				So(err, ShouldBeNil)
+
+				secondStorySave := entities.StorySave{
+					Token:     "OrAskedTheGrinningBobcatWhyHeGrinned",
+					Save:      database.Jsonb{json.RawMessage(rawBytes)},
+					CreatedBy: user.ID,
+					BookId:    book.ID,
+					ChapterId: chapter.ID,
+				}
+				err = storySaveRepo.Insert(&secondStorySave)
+				So(err, ShouldBeNil)
+
+
+				Convey("Finding the newly created Save using the token should work", func (){
+					searchResult, err := storySaveRepo.FindByToken(storySave.Token)
+					So(err, ShouldBeNil)
+					So(searchResult.Save.ToString(), ShouldEqual, storySave.Save.ToString())
+				})
+
+				Convey("Updating the created Save should work", func (){
+					rawBytes, err = json.Marshal(map[string]interface{} {
+						"have_heard_the_wolf_cry_to_the_blue_corn_moon" : false,
+					})
+					So(err, ShouldBeNil)
+
+					storySave.Save = database.Jsonb{ json.RawMessage(rawBytes) }
+					err = storySaveRepo.Update(&storySave)
+					So(err, ShouldBeNil)
+
+					Convey("Finding the updated Save should yield a result with updated fields", func (){
+						searchResult, err := storySaveRepo.FindByToken(storySave.Token)
+						So(err,ShouldBeNil)
+						So(searchResult.Save.ToString(), ShouldEqual, storySave.Save.ToString())
+					})
+				})
+
+
+				Convey("Deleting the created Save should work", func (){
+					err = storySaveRepo.Delete(&storySave)
+					So(err, ShouldBeNil)
+
+					Convey("Finding the deleted Save should yield no results", func (){
+						_, err := storySaveRepo.FindByToken(storySave.Token)
+						So(err,ShouldNotBeNil)
+						So(err.EndpointMessage(), ShouldEqual, "No save found")
+					})
+
+					Convey("Finding the Save that wasn't deleted should still yield a result", func(){
+						_, err := storySaveRepo.FindByToken(secondStorySave.Token)
+						So(err, ShouldBeNil)
+					})
+				})
+
+			})
+		})
+
+
+		Reset(func (){
+			transaction.Rollback()
+			So(transaction.Error,ShouldBeNil)
+		})
+	}))
+
+}
