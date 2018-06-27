3da15efe021eec1ea74fdc5bb630983cf4a2cf19
Adrian <adrianhyk@gmail.com>
2018-06-27 21:06:08 +0800

    more explicit message

--- http/middleware/auth_middleware/AuthMiddleware.go
+++ http/middleware/auth_middleware/AuthMiddleware.go
@@ -37,7 +37,7 @@ func New(getLoggedInUser auth_domain.GetLoggedInUser) AuthMiddleware {
 
 		if len(token) == 0 {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H {
-				"message" : "No login token provided!",
+				"message" : "An authorization token is required to access this",
 			})
 			return
 		}
